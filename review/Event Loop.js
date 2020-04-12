c5130e2e6df44c68de410346addfeab224f9c343
雷洪兵 <leihb@dajiashequ.com>
2020-04-12 21:13:51 +0800

    2020-4-14提交

--- review/Event Loop.js
+++ review/Event Loop.js
@@ -37,28 +37,45 @@
 
 
 
-console.log(1)
+// console.log(1)
+//
+// setTimeout(() => {
+//   console.log(2)
+//   new Promise(resolve => {
+//     console.log(4)
+//     resolve()
+//   }).then(() => {
+//     console.log(5)
+//   })
+//   process.nextTick(() => {
+//     console.log(3)
+//   })
+// })
+//
+// new Promise(resolve => {
+//   console.log(7)
+//   resolve()
+// }).then(() => {
+//   console.log(8)
+// })
+//
+// process.nextTick(() => {
+//   console.log(6)
+// })
 
-setTimeout(() => {
-  console.log(2)
-  new Promise(resolve => {
-    console.log(4)
-    resolve()
-  }).then(() => {
-    console.log(5)
-  })
-  process.nextTick(() => {
-    console.log(3)
-  })
-})
 
-new Promise(resolve => {
-  console.log(7)
-  resolve()
-}).then(() => {
-  console.log(8)
-})
+function test () {
+  console.log('start')
+  setTimeout(() => {
+    console.log('children2')
+    Promise.resolve().then(() => {console.log('children2-1')})
+  }, 0)
+  setTimeout(() => {
+    console.log('children3')
+    Promise.resolve().then(() => {console.log('children3-1')})
+  }, 0)
+  Promise.resolve().then(() => {console.log('children1')})
+  console.log('end')
+}
 
-process.nextTick(() => {
-  console.log(6)
-})
+test()
